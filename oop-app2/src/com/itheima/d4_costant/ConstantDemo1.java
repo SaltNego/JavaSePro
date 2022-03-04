1aafd625f83241f31ae487807d706b0a639a824e
SaltNego <1053431697@qq.com>
2022-03-04 20:54:46 +0800

    2022.0304

---
+++ oop-app2/src/com/itheima/d4_costant/ConstantDemo1.java
@@ -0,0 +1,25 @@
+package com.itheima.d4_costant;
+
+/**
+ * @author： Negoowen
+ * @date： 2022-03-04
+ * @project_name: javase_pro
+ * /**
+ *  * 学会常量的使用，并理解常量
+ */
+public class ConstantDemo1 {
+    public static final String SCHOOL_NAME = "清华一中";
+    public static final String USER_NAME = "梅花朵朵";
+    public static final String PASS_WORD ="123456";
+//    public final String NAME = "清华一中";
+
+    public static void main(String[] args) {
+        System.out.println(SCHOOL_NAME);
+        System.out.println(USER_NAME);
+
+        if (USER_NAME.equals("")) {
+
+        }
+    }
+
+}
