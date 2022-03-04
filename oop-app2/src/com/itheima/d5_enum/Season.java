b4335fcd77951bfb08dc0c7d7face501131a8b5b
SaltNego <1053431697@qq.com>
2022-03-04 22:39:27 +0800

    2022.0304

---
+++ oop-app2/src/com/itheima/d5_enum/Season.java
@@ -0,0 +1,12 @@
+package com.itheima.d5_enum;
+
+/**
+ * @author： Negoowen
+ * @date： 2022-03-04
+ * @project_name: javase_pro
+ * 枚举类
+ */
+public enum Season {
+    SPRING,SUMMER,AUTUMN, WINTER;
+
+}
